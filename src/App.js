e2da81c9f4af89ee5ece772c88bb243fbd45a7fe
Abbas <abbasialimohd@gmail.com>
2021-03-27 00:38:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import './SASS/style.css'
 
 function App() {
+  const key = '273ea6c9087c4da3890202900211903';
+  const [name,setName] = useState([]);
+  const [typing,setTyping] = useState((""));
+  const [full,setFull] = useState((""));
+
+  const inputEvent = (event) => {
+    setTyping(event.target.value)
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFull(typing);
+  }
+  
+
+  useEffect(()=>{
+    async function getData(){
+      const res = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${full}&days=1&aqi=no&alerts=no`);
+      setName(res.data);      
+      console.log(res.data)
+    }
+    getData();
+  },[full])
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+       <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-sm-12 col-xs-12 Box">
+            <form>
+              <input type="text" placeholder="Search" onChange={inputEvent}/>
+              <button onClick={onSubmit}>Search</button>
+              </form>
+              {(typeof  name.location != "undefined")  ? (
+                <div>
+              <h3>{name.location.name} , {name.location.country}</h3>
+              <h5>{name.location.localtime}</h5>
+              <div className="degree"><h1>{name.current.temp_c}°C</h1></div>
+              <h4>{name.current.condition.text}</h4>
+              </div>
+              ) : ( <p className="nofound">No Data Found</p>
+              )}
+      </div>
+      </div>
     </div>
   );
 }
